cc8da069498ba2f3ad16a7b8268faf16d5ab5cee
sheng.wang <justwangsheng@gmail.com>
2023-11-30 21:48:47 +0800

    feat: add ticket flag

--- cmd/root.go
+++ cmd/root.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"github.com/starudream/go-lib/cobra/v2"
+	"github.com/starudream/go-lib/core/v2/config"
+	"github.com/starudream/go-lib/core/v2/utils/osutil"
 )
 
 var rootCmd = cobra.NewRootCommand(func(c *cobra.Command) {
 	c.Use = "aliyunxiao-generator"
+
+	c.PersistentFlags().String("ticket", "", "aliyun ticket")
+	osutil.Must0(c.MarkPersistentFlagRequired("ticket"))
+
+	c.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		config.LoadFlags(c.PersistentFlags())
+	}
 })
